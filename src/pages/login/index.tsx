b5305eeb3ba25125560771283fb9637a1f17ebdf
Sahilbalhra <sahilbalhra0@gmail.com>
2024-06-20 23:20:42 +0530

    added login functionality

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -8,17 +8,31 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useMutation } from "@tanstack/react-query";
 import { useRef } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { loginApi } from "@/api/apiCore";
 
 const Login = () => {
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
+  const navigate = useNavigate();
+
+  const mutation = useMutation({
+    mutationFn: loginApi,
+    onSuccess: () => {
+      navigate("/dashboard/home");
+    },
+  });
+
   const handleLoginSubmit = () => {
     const email = emailRef.current?.value;
     const password = passwordRef.current?.value;
     console.log(email, password);
+    if (email && password) {
+      mutation.mutate({ email, password });
+    }
   };
 
   return (
